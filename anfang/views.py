2fbda7b23f6149a6bcad561614020de6bac87dc5
Neal Sidhwaney <nealsid+github@gmail.com>
2014-10-04 19:24:51 -0400

    New user registration page, user profile, jQuery wireup

--- anfang/views.py
+++ anfang/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
+from models import STIIUserProfile
 
 # Create your views here.
 def index(request):
     return render(request, 'anfang/index.html')
 
+def new_user_create(request):
+    return HttpResponse("Not impl")
+
+def new_user(request):
+    return render(request, "anfang/new-user.html")
+
 @login_required
-def main_page(request):
+def start(request):
+    u = request.user
+    if hasattr(u, "stiiuserprofile"):
+        profile = u.stiiuserprofile
+        return HttpResponse("hello, %(username)s, you are in a relationship with %(reluser)s" % { "username":str(u), "reluser":str(profile.relationship)})
+    else:
+        return HttpResponse("Hello, %(username)s, you are NOT in a relationship with anyone" % {"username":str(u)})
 
 def process_login(request):
     email = request.POST['email']
@@ -17,9 +30,6 @@ def process_login(request):
     user = authenticate(username=email, password=password)
     if user is not None:
         login(request, user)
-        return HttpResponse('hello, %(email)s' % {'email':user.email})
+        return HttpResponseRedirect("/anfang/start")
     else:
         return HttpResponse('hello, world!')
-
-
-
