c74a6ded8a58968152d5cd42a4e2e4fd1e15cec1
Neal Sidhwaney <nealsid+github@gmail.com>
2014-10-06 21:56:36 -0400

    Fix relationship queries

--- anfang/models.py
+++ anfang/models.py
@@ -6,6 +6,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
     primary_relationship = models.ForeignKey('UserRelationship')
     relationships = models.ManyToManyField('UserRelationship', related_name="secondary")
+    def __unicode__(self):
+        return "%(email)s in a relationship with id: %(id)d" % {"email":self.user.email,"id":self.primary_relationship_id}
 
 class UserRelationship(models.Model):
     title = models.CharField(max_length = 25)
--- anfang/views.py
+++ anfang/views.py
@@ -14,9 +14,39 @@ def index(request):
 def start(request):
     u = request.user
     if hasattr(u, "userprofile"):
-        profile = u.userprofile
-        return HttpResponse("hello, %(username)s, you are in a relationship with %(reluser)s" % { "username":str(u), "reluser":str(profile.relationship)})
+        primary_rel = u.userprofile.primary_relationship
+
+        other_user = User.objects.filter(userprofile__primary_relationship=primary_rel).exclude(id=u.id)
+        potential_users = None
     else:
-        context = {'primaryreluser':None}
-        return render(request, "anfang/start-page.html")
-        return HttpResponse("Hello, %(username)s, you are NOT in a relationship with anyone" % {"username":str(u)})
+        other_user = None
+        potential_users = User.objects.filter(userprofile = None).exclude(id = u.id)
+    context = {
+        'primaryreluser':other_user,
+        'potential_users':potential_users,
+    }
+    return render(request, "anfang/start-page.html", context)
+
+@login_required
+def mark_relationship(request):
+    other_user_id = request.GET['with_id']
+    dst_user = User.objects.get(id=other_user_id)
+    user = request.user
+
+    # There is a very particular order here, because Django's APi
+    # seems to require objects to be saved before they can be
+    # referenced by another object.  So we essentially have to
+    # create/save the objects from the order of: 1) UserRelationship(1
+    # instance), UserProfile (2 instances), and User(2 instances)
+    rel = UserRelationship(title=u'primary')
+    rel.save()
+
+    user.userprofile = UserProfile(user=user, primary_relationship = rel)
+    user.userprofile.save()
+    user.save()
+
+    dst_user.userprofile = UserProfile(user=dst_user, primary_relationship = rel)
+    dst_user.userprofile.save()
+    dst_user.save()
+
+    return HttpResponseRedirect("/anfang/start")
