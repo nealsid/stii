49cfbad7fdba8fe58289dcd12385ab3b3ea5d542
Neal Sidhwaney <nealsid+github@gmail.com>
2014-10-06 21:56:36 -0400

    Fix relationship queries

--- anfang/urls.py
+++ anfang/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.index, name='index'),
     url(r'^start$', views.start, name='start'),
+    url(r'^mark-relationship$', views.mark_relationship, name="mark_relationship"),
 )
