a9d1158fc9a28d943c758e906d70c56ff08f149d
Neal Sidhwaney <nealsid+github@gmail.com>
2014-10-31 18:32:37 -0400

    Tweaks to crop viewport widget - viewport and shade widget now track each other correctly.

--- anfang/static/js/croptastic.js
+++ anfang/static/js/croptastic.js
@@ -17,43 +17,57 @@ function Croptasticr(parentNode) {
 }
 
 Croptasticr.prototype.pointsToSVGPolygonString = function(points) {
-  var svgstring = "M" + points[0]['x'] + "," + points[0]['y'];
+  var svgstring = "M" + points[0]['x'] + "," + points[0]['y'] + " ";
   for (var i = 1; i < points.length; i++) {
-    svgstring += "L" + points[i]['x'] + "," + points[i]['y'];
+    svgstring += "L" + points[i]['x'] + "," + points[i]['y'] + " ";
   }
   svgstring += "Z";
   return svgstring;
 };
 
 Croptasticr.prototype.drawViewport = function() {
-
-};
-
-Croptasticr.prototype.setupViewport = function() {
-  var strokeColor = "#949393";
-  var polyFill = "#949393";
-  var fillOpacity = .7;
-
-  var outerPolyPoints = [{'x' : 0, 'y' : 0},
-			 {'x' : this.width, 'y' : 0},
-			 {'x' : this.width, 'y' : this.height},
-			 {'x' : 0, 'y' : this.height}];
-  if (this.viewportElement != null) {
-    this.viewportElement.remove();
-    this.viewportElement = null;
-  }
-
-  if (this.shadeElement != null) {
-    this.shadeElement.remove();
-    this.shadeElement = null;
-  }
-
   var centerX = this.viewportCenterX;
   var centerY = this.viewportCenterY;
   var viewport_ul = {'x' : centerX - 50, 'y' : centerY - 50};
   var viewport_ur = {'x' : centerX + 50, 'y' : centerY - 50};
   var viewport_lr = {'x' : centerX + 50, 'y' : centerY + 50};
   var viewport_ll = {'x' : centerX - 50, 'y' : centerY + 50};
+  var innerPolyPoints = [viewport_ul,
+			 viewport_ur,
+			 viewport_lr,
+			 viewport_ll];
+  var viewportSVG = this.pointsToSVGPolygonString(innerPolyPoints);
+  this.viewportElement = this.paper.path(viewportSVG).attr("fill",
+							   "transparent");
+  console.log("viewport: " + viewportSVG);
+};
+
+Croptasticr.prototype.moveInnerViewport = function(dx, dy) {
+  var xformString = "t" + dx + "," + dy;
+  this.viewportElement.transform("..." + xformString);
+  console.log("transform SVG: " + xformString);
+  console.log("current xform: " + this.viewportElement.transform());
+};
+
+Croptasticr.prototype.drawShadeElement = function() {
+  if (this.shadeElement != null) {
+    this.shadeElement.remove();
+    this.shadeElement = null;
+  }
+  var strokeColor = "#949393";
+  var polyFill = "#949393";
+  var fillOpacity = .7;
+  var centerX = this.viewportCenterX;
+  var centerY = this.viewportCenterY;
+  console.log("shade element cutout center: (" + this.viewportCenterX + "," + this.viewportCenterY + ")");
+  var viewport_ul = {'x' : centerX - 50, 'y' : centerY - 50};
+  var viewport_ur = {'x' : centerX + 50, 'y' : centerY - 50};
+  var viewport_lr = {'x' : centerX + 50, 'y' : centerY + 50};
+  var viewport_ll = {'x' : centerX - 50, 'y' : centerY + 50};
+  var outerPolyPoints = [{'x' : 0, 'y' : 0},
+			 {'x' : this.width, 'y' : 0},
+			 {'x' : this.width, 'y' : this.height},
+			 {'x' : 0, 'y' : this.height}];
   // Note the order of the points - it's required to go counter
   // clockwise with Raphael so that it considers this a subtraction
   // from the outer poly.
@@ -65,24 +79,41 @@ Croptasticr.prototype.setupViewport = function() {
   var polySVG = this.pointsToSVGPolygonString(outerPolyPoints);
   polySVG += this.pointsToSVGPolygonString(innerPolyPoints);
   this.shadeElement = this.paper.path(polySVG).attr("fill",polyFill).attr("opacity", fillOpacity);
+  console.log(polySVG);
+};
 
-  var viewportPoints = innerPolyPoints.reverse();
-  var viewportSVG = this.pointsToSVGPolygonString(viewportPoints);
-  this.viewportElement = this.paper.path(viewportSVG).attr("fill","transparent");
 
+Croptasticr.prototype.setupViewport = function() {
+  if (this.viewportElement != null) {
+    this.viewportElement.remove();
+    this.viewportElement = null;
+  }
+
+  if (this.shadeElement != null) {
+    this.shadeElement.remove();
+    this.shadeElement = null;
+  }
+  console.log("initial viewport center: (" + this.viewportCenterX + "," + this.viewportCenterY + ")");
+
+  this.drawShadeElement();
+  this.drawViewport();
   var croptasticr = this;
   this.viewportElement.drag(function(dx, dy, x, y, e) {
-    croptasticr.viewportCenterX = x - croptasticr.xoffset;
-    croptasticr.viewportCenterY = y - croptasticr.yoffset;
-    croptasticr.setupViewport();
+    var realDX = (x - croptasticr.lastx);
+    var realDY = (y - croptasticr.lasty);
+    croptasticr.viewportCenterX += realDX;
+    croptasticr.viewportCenterY += realDY;
+    croptasticr.lastx = x;
+    croptasticr.lasty = y;
+    console.log("new viewport center: (" + croptasticr.viewportCenterX + "," + croptasticr.viewportCenterY + ")");
+//    console.log(croptasticr.viewportCenterX + " / " + croptasticr.viewportCenterY + " / " + (croptasticr.viewportCenterX + dx) + " / " + (croptasticr.viewportCenterY + dy) + " / " + (x - croptasticr.xoffset) + " / " + (y - croptasticr.yoffset));
+
+    croptasticr.moveInnerViewport(realDX, realDY);
+    croptasticr.drawShadeElement();
   }, function(x, y, e) {
-    croptasticr.startx = x - croptasticr.xoffset;
-    croptasticr.starty = y - croptasticr.yoffset;
-  }, function(xe) {
-    console.log("moving x by " + (croptasticr.finalx - croptasticr.startx) + " and y by: " + (croptasticr.finaly - croptasticr.starty));
+    croptasticr.lastx = x;
+    croptasticr.lasty = y;
   });
-  console.log(polySVG);
-  console.log(viewportSVG);
 };
 
 Croptasticr.prototype.setup = function(pic_url) {
